61bbf683b0fc871dd9672468bd63b188a76039fa
unknown <prateeksharma2612@gmail.com>
2021-05-31 08:32:00 +0530

    Updated menu item component and lokking for Higher order Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import {Switch, Route} from 'react-router-dom';
+
 import './App.css';
+
+const HatsPage = () =>
+{
+  return(
+    <div>Hats Page </div>
+  )
+}
+
 
 function App() {
   return (
-    <div >
-        <HomePage />
-    </div>
+    
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/Hats' component= {HatsPage} />
+    </Switch>
+    
   );
 }
 
